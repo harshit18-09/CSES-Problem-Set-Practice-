3954c91ea2b186f2c6550849801afa8b3a9d35a6
HC18 <harshit.chugh18@gmail.com>
2025-09-24 05:56:51 +0530

    24 september 2025

---
+++ IntroductoryProblems/TwoSets.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    
+    int sum = n*(n+1)/2;
+    if(sum % 2 == 0){
+        int h = sum/2;
+        
+    }
+    return 0;
+}
